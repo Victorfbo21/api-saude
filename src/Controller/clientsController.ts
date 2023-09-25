4255b1a428409e8453cb86a7bcbbfd0b8a4b5f54
Victor Bentes <victorbentesdev@gmail.com>
2023-09-24 21:39:47 -0300

    edit clients Controller and clients Model

--- src/Controller/clientsController.ts
+++ src/Controller/clientsController.ts
@@ -3,56 +3,80 @@ import { ICreateClient } from 'Interfaces/createClient.interface';
 import { IHealthProblems } from 'Interfaces/health-problems.interface';
 
 const insertClient = async (req: any, res: any) => {
-    const client = req.body;
-    const created = await ClientModel.insertClient(client)
-    if (created) {
-        res.status(201).send(created)
-    }
-    else {
-        res.send(500)
+    try {
+        const client = req.body;
+        const created = await ClientModel.insertClient(client);
+        if (created) {
+            res.status(201).send(created);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch (error) {
+        console.error('Error inserting client:', error);
+        res.sendStatus(500);
     }
 }
 
 const getClients = async (req: any, res: any) => {
-    const find = await ClientModel.getClients(req.query.filter, req.query.skip, req.query.limit)
-    if (find) {
-        res.status(200).send(find)
-    } else {
-        res.status(500)
+    try {
+        const find = await ClientModel.getClients(req.query.filter, req.query.skip, req.query.limit);
+        if (find) {
+            res.status(200).send(find);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch (error) {
+        console.error('Error getting clients:', error);
+        res.sendStatus(500);
     }
 }
 
 const deleteClient = async (req: any, res: any) => {
-    const id = req.query.id
-    const clientDeleted = await ClientModel.deleteClient(id)
-    if (clientDeleted) {
-        res.send('Usuário :', clientDeleted._id, 'Foi Deletado')
-        res.sendStatus(200)
-    } else {
-        res.send(500)
+    try {
+        const id = req.query.id;
+        const clientDeleted = await ClientModel.deleteClient(id);
+        if (clientDeleted) {
+            res.send(`Usuário ${clientDeleted._id} Foi Deletado`);
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch (error) {
+        console.error('Error deleting client:', error);
+        res.sendStatus(500);
     }
 }
 
 const updateClient = async (req: any, res: any) => {
-    const id = req.query.id
-    const update: Partial<ICreateClient> = req.body
-    const clientUpdate = await ClientModel.updateClient(id, update)
-    if (clientUpdate) {
-        res.sendStatus(201)
-        res.send(clientUpdate)
-    } else {
-        res.sendStatus(500)
+    try {
+        const id = req.query.id;
+        const update: Partial<ICreateClient> = req.body;
+        const clientUpdate = await ClientModel.updateClient(id, update);
+        if (clientUpdate) {
+            res.sendStatus(201);
+            res.send(clientUpdate);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch (error) {
+        console.error('Error updating client:', error);
+        res.sendStatus(500);
     }
 }
 
 const addHealthProblem = async (req: any, res: any) => {
-    const id = req.query.id
-    const update: IHealthProblems = req.body
-    const clientUpdate = await ClientModel.addHealthProblem(id, update)
-    if (clientUpdate) {
-        res.send(clientUpdate)
-    } else {
-        res.sendStatus(500)
+    try {
+        const id = req.query.id;
+        const update: IHealthProblems = req.body;
+        const clientUpdate = await ClientModel.addHealthProblem(id, update);
+        if (clientUpdate) {
+            res.send(clientUpdate);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch (error) {
+        console.error('Error adding health problem to client:', error);
+        res.sendStatus(500);
     }
 }
 
--- src/Model/clientModel.ts
+++ src/Model/clientModel.ts
@@ -3,95 +3,88 @@ import ClientSchema from "../Schemas/clientSchema";
 import { ICreateClient } from "Interfaces/createClient.interface";
 
 const insertClient = async (user: ICreateClient) => {
-    const clientCreated = new ClientSchema({ ...user })
-    return await clientCreated.save().then(
-        (o) => {
-            console.log('Client Saved Successfully!')
-            return o
-        }
-
-    ).catch(
-        (e) => {
-            return e
-        }
-    )
-}
+    try {
+        const clientCreated = new ClientSchema({ ...user });
+        const savedClient = await clientCreated.save();
+        console.log('Client Saved Successfully!');
+        return savedClient;
+    } 
+    catch (e) {
+        console.log('Error saving client:');
+        console.log(e);
+        throw e;
+    }
+};
 
 const getClients = async (filter: string, skip: number, limit: number) => {
-    filter = filter || ''
-    return await ClientSchema.find({
-        $or: [
-            { name: new RegExp('.*' + filter + '.*', 'i') },
-            { dateOfBirth: new RegExp('.*' + filter + '.*', 'i') },
-            { gender: new RegExp('.*' + filter + '.*', 'i') },
-
-        ]
-    }).skip(skip || 0).limit(limit || 0).then(
-        (o) => {
-            return o
-        }
-    ).catch(
-        (e) => {
-            console.log('Error Finding Users', e)
-            return null
-        }
-    )
-}
+    try {
+        filter = filter || '';
+        const clients = await ClientSchema.find({
+            $or: [
+                { name: new RegExp('.*' + filter + '.*', 'i') },
+                { dateOfBirth: new RegExp('.*' + filter + '.*', 'i') },
+                { gender: new RegExp('.*' + filter + '.*', 'i') },
+            ],
+        })
+            .skip(skip || 0)
+            .limit(limit || 0);
+        return clients;
+    } catch (e) {
+        console.log('Error Finding Users', e);
+        return null;
+    }
+};
 
 
 const deleteClient = async (id: string) => {
-    return await ClientSchema.findByIdAndRemove(id).then(
-        (o) => {
-            return o
-        }
-    ).catch(
-        (e) => {
-            console.log('Error on Delete User')
-        }
-
-    )
+    try {
+        const deletedClient = await ClientSchema.findByIdAndRemove(id);
+        return deletedClient;
+    } catch (e) {
+        console.log('Error on Delete User:', e);
+    }
 }
 
 const updateClient = async (id: string, update: Partial<ICreateClient>) => {
-
-    return await ClientSchema.findByIdAndUpdate(id, update)
-        .then(
-            (o) => {
-                return o
-            }
-        ).catch(
-            (err) => {
-                console.log('Error on Updated User')
-                console.log(err)
-            }
-
-        )
+    try {
+        const updatedClient = await ClientSchema.findByIdAndUpdate(id, update);
+        return updatedClient;
+    } catch (err) {
+        console.log('Error on Updated User:', err);
+    }
 }
 
 const addHealthProblem = async (id: string, update: IHealthProblems) => {
-    const user = await ClientSchema.findById(id)
-    const previousHealthProblems = user?.healthProblems
-    const existingProblem = previousHealthProblems?.find(problem => problem.name?.toLowerCase() === update.problem_name?.toLowerCase() && problem.degree?.toLowerCase() === update.degree?.toLowerCase())
-    if (!existingProblem) {
-        previousHealthProblems?.push(update)
-    }
-    else {
-        console.log("Erro ao Adicionar Problema de Saúde")
-    }
+    try {
+        const user = await ClientSchema.findById(id);
+        if (!user) {
+            throw new Error("Usuário não Cadastrado em Nossa Base de Dados");
+        }
 
-    return await ClientSchema.findByIdAndUpdate(id, { healthProblems: previousHealthProblems })
-        .then(
-            (o) => {
-                return o
-            }
-        ).catch(
-            (err) => {
-                console.log('Error on Updated User')
-                console.log(err)
-            }
+        const previousHealthProblems = user?.healthProblems;
+        const existingProblem = previousHealthProblems?.find(
+            (problem) =>
+                problem.name?.toLowerCase() === update.problem_name?.toLowerCase() &&
+                problem.degree?.toLowerCase() === update.degree?.toLowerCase()
+        );
 
-        )
-}
+        if (!existingProblem) {
+            previousHealthProblems?.push(update);
+        } else {
+            console.log("Erro ao Adicionar Problema de Saúde");
+        }
+
+        const updatedUser = await ClientSchema.findByIdAndUpdate(id, {
+            healthProblems: previousHealthProblems,
+        });
+
+        return updatedUser;
+    } catch (err) {
+        console.log('Error on Updated User');
+        console.log(err);
+        throw err; 
+    }
+};
 
 
 export default {
